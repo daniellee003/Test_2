6b11200b5ac1c03c466c17ad7c6e48b861511d85
daniellee003 <daniellee003@gmail.com>
2021-01-27 11:09:44 -0500

    Initial commit

---
+++ src/com/company/Hello.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Georgetown");
+
+    }
+}
